cdd3b90c3f48d77327d2ed74f4f8bd7283f3354b
kaiokassiano <kaio.kassiano.oliveira@ccc.ufcg.edu.br>
2016-11-23 00:50:58 -0200

    montando a estrutura basica da view

---
+++ lab2/assets/js/main.js
@@ -0,0 +1,10 @@
+var app = angular.module("lab2", []);
+
+app.controller("lab2controller", function($scope) {
+	$scope.app = "Lab 2";
+	$scope.tarefas = [];
+
+	$scope.adicionarTarefa = function (tarefa) {
+		
+	};
+});
