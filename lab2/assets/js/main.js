2878a90aecf32641e01d0fe85e8b318115f6e9c0
kaiokassiano <kaio.kassiano.oliveira@ccc.ufcg.edu.br>
2016-11-23 01:45:09 -0200

    ta funcionando adicionar novas entradas

--- lab2/assets/js/main.js
+++ lab2/assets/js/main.js
@@ -2,9 +2,13 @@ var app = angular.module("lab2", []);
 
 app.controller("lab2controller", function($scope) {
 	$scope.app = "Lab 2";
-	$scope.tarefas = [];
+	$scope.tarefas = [
+	];
 
 	$scope.adicionarTarefa = function (tarefa) {
-		
+		if (tarefa != "") {
+			$scope.tarefas.push(angular.copy(tarefa));
+			delete $scope.tarefa;
+		}
 	};
 });
