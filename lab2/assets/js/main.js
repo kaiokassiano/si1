1d4389cb12bf969877136e0dbfa2287d589ba170
kaiokassiano <kaio.kassiano.oliveira@ccc.ufcg.edu.br>
2016-12-07 11:30:19 -0200

    troquei tudo pra angular material. falta poder adicionar os TODO

--- lab2/assets/js/main.js
+++ lab2/assets/js/main.js
@@ -1,17 +1,48 @@
 var app = angular.module("lab2", ['ngMaterial']);
 
-app.controller("lab2controller", function($scope) {
+app.controller("lab2controller", function ($scope) {
 	$scope.app = "Lab 2";
-	$scope.tarefas = [
-		{ descricao: 'asdasdasd', concluido: false},
-    { descricao: 'ijnkhnhnhnnhk', concluido: false },
-    { descricao: 'asdasdyh', concluido: false }
-	];
 
-	$scope.adicionarTarefa = function (tarefa) {
-		if (tarefa !== undefined) {
-			$scope.tarefas.push(angular.copy(tarefa));
-			delete $scope.tarefa;
+	$scope.tarefas = [{
+		descricao: 'Levar o cachorro pra passear',
+		concluido: false
+	}, {
+		descricao: 'Estudar lógica',
+		concluido: false
+	}, {
+		descricao: 'Terminar o Lab de SI',
+		concluido: false
+	}];
+
+	$scope.tarefasSelecionadas = [];
+
+	$scope.toggle = function (item, list) {
+		var idx = list.indexOf(item);
+		if (idx > -1) {
+			list.splice(idx, 1);
+		} else {
+			list.push(item);
+		}
+	};
+
+	$scope.exists = function (item, list) {
+		return list.indexOf(item) > -1;
+	};
+
+	$scope.isIndeterminate = function () {
+		return ($scope.tarefasSelecionadas.length !== 0 &&
+			$scope.tarefasSelecionadas.length !== $scope.tarefas.length);
+	};
+
+	$scope.isChecked = function () {
+		return $scope.tarefasSelecionadas.length === $scope.tarefas.length;
+	};
+
+	$scope.toggleAll = function () {
+		if ($scope.tarefasSelecionadas.length === $scope.tarefas.length) {
+			$scope.tarefasSelecionadas = [];
+		} else if ($scope.tarefasSelecionadas.length === 0 || $scope.tarefasSelecionadas.length > 0) {
+			$scope.tarefasSelecionadas = $scope.tarefas.slice(0);
 		}
 	};
 });
